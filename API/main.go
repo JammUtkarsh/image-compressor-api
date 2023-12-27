467e16c20def552268dae67b50681313e8dc54b8
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2023-12-27 17:46:52 +0530

    Setup Go ORM with Postgres
    I am using an ORM here because, in many previous projects I have used executed raw SQL and this was something new to me so I wanted to give it a try
    
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- API/main.go
+++ API/main.go
@@ -9,14 +9,6 @@ import (
 	"net/url"
 )
 
-type Product struct {
-	UserID             int64    `json:"user_id"`
-	ProductName        string   `json:"product_name"`
-	ProductDescription string   `json:"product_description"`
-	ProductImages      []string `json:"product_images"`
-	ProductPrice       float64  `json:"product_price"`
-}
-
 func main() {
 	http.HandleFunc("/", AddProducts)
 	fmt.Println("Server listening on: http://127.0.0.1:8080")
--- API/models.go
+++ API/models.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        int
+	Name      string  `json:"name"`
+	Mobile    string  `json:"mobile"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Product struct {
+	ProductID               int64    `json:"product_id"`
+	UserID                  int64    `json:"user_id"`
+	ProductName             string   `json:"product_name"`
+	ProductDescription      string   `json:"product_description"`
+	ProductImages           []string `json:"product_images"`
+	ProductPrice            float64  `json:"product_price"`
+	CompressedProductImages []string
+	CreatedAt               time.Time
+	UpdatedAt               time.Time
+}
+
+var db *gorm.DB
+
+var (
+	port     = 5432
+	host     = os.Getenv("POSTGRES_HOST")
+	user     = os.Getenv("POSTGRES_USER")
+	password = os.Getenv("POSTGRES_PASSWORD")
+	dbname   = os.Getenv("POSTGRES_DB")
+)
+
+func Connect() (err error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err = gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return
+}
+
 func userExists(userID int64) bool {
 	return true
 }
