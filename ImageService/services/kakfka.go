ee93844035255182163a0c1700762c5c951e4b4e
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2023-12-30 22:31:45 +0530

    minor code refactor
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- ImageService/services/kakfka.go
+++ ImageService/services/kakfka.go
@@ -13,6 +13,8 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
+// The function establishes a connection to a Kafka broker using the provided topic and brokers, or
+// defaults to localhost:29092 if not provided.
 func KConfig() *kafka.Conn {
 	topic := os.Getenv("KAFKA_TOPIC")
 	brokers := os.Getenv("KAFKA_BROKERS")
@@ -27,6 +29,9 @@ func KConfig() *kafka.Conn {
 	return conn
 }
 
+// The function `KConsumer` reads messages from a Kafka topic, retrieves image URLs from a database
+// based on the message content, compresses the images, and saves the compressed images back to the
+// database.
 func KConsumer(db *sql.DB) {
 	conn := KConfig()
 	defer conn.Close()
@@ -44,7 +49,7 @@ func KConsumer(db *sql.DB) {
 			if err != nil {
 				log.Println(err)
 			}
-			compImg := WriteToDisk(db, urls)
+			compImg := WriteToDisk(urls)
 			if err = AddCompImg(db, userID, productID, compImg); err != nil {
 				log.Println(err)
 			}
@@ -53,6 +58,8 @@ func KConsumer(db *sql.DB) {
 	}
 }
 
+// This takes a byte array representing a JSON message and returns the user ID
+// and product ID extracted from the message.
 func MsgUnarshal(kMsg []byte) (userID, productID int) {
 	msg := struct {
 		UserID    int `json:"u"`
@@ -62,25 +69,30 @@ func MsgUnarshal(kMsg []byte) (userID, productID int) {
 	if err := json.Unmarshal(kMsg, &msg); err != nil {
 		return -1, -1
 	}
-
 	return msg.UserID, msg.ProductID
 }
 
-func WriteToDisk(db *sql.DB, urls []string) (compImages []string) {
+// The function takes a database connection and a list of URLs, downloads and compresses the images
+// from the URLs, saves them to disk, and returns a list of the file paths of the compressed images.
+func WriteToDisk(urls []string) (compImages []string) {
 	var img Image
 	for _, url := range urls {
 		if err := img.ImgURLToBuffer(url); err != nil {
 			log.Println(err)
 		}
 
-		if err := img.ImgCompress(80, bimg.JPEG); err != nil {
+		if err := img.ImgCompress(50, bimg.JPEG); err != nil {
 			log.Println(err)
 		}
 		err := img.SaveFile("../compressed")
 		if err != nil {
 			log.Println(err)
 		}
-		fPath := filepath.Join("../compressed", img.Name)
+		pwd, err := os.Getwd()
+		if err != nil {
+			log.Println(err)
+		}
+		fPath := filepath.Join(pwd, "../compressed", img.Name)
 		compImages = append(compImages, filepath.Clean(fPath))
 		log.Printf("Image %s saved to disk\n", img.Name)
 	}
