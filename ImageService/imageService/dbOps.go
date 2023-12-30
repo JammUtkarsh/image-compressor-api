60c950eb28cf3a93239e37bc56a34eff5b3fc9a8
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2023-12-30 14:36:19 +0530

    Kafka Consumer added
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

---
+++ ImageService/imageService/dbOps.go
@@ -0,0 +1,61 @@
+package imageservice
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/lib/pq"
+)
+
+const (
+	GetImageArrayQuery = "SELECT product_images FROM products WHERE id = $1 AND user_id = $2"
+	AddCompImageQuery  = "UPDATE products SET compressed_product_images = $1 WHERE id = $2 AND user_id = $3"
+)
+
+var (
+	port     = 5432
+	host     = os.Getenv("POSTGRES_HOST")
+	user     = os.Getenv("POSTGRES_USER")
+	password = os.Getenv("POSTGRES_PASSWORD")
+	dbname   = os.Getenv("POSTGRES_DB")
+)
+
+// func init() {
+// 	if host == "" || user == "" || password == "" || dbname == "" {
+// 		log.Fatalln("Please set the postgres enviroment variables")
+// 	}
+// }
+
+func Connect() (db *sql.DB, err error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return
+	}
+	for i := 0; i < 5; i++ {
+		if err = db.Ping(); err != nil {
+			log.Println("failed to connect to database, retrying...")
+			time.Sleep(6 * time.Second)
+		} else {
+			return
+		}
+	}
+	if err != nil {
+		fmt.Println("failed to connect to database")
+		os.Exit(1)
+	}
+	return
+}
+
+func GetImgArray(db *sql.DB, id int, userID int) (imageArray []string, err error) {
+	err = db.QueryRow(GetImageArrayQuery, id, userID).Scan(pq.Array(&imageArray))
+	return
+}
+
+func AddCompImg(db *sql.DB, id int, userID int, compImage []string) (err error) {
+	_, err = db.Exec(AddCompImageQuery, pq.Array(compImage), id, userID)
+	return
+}
--- ImageService/main.go
+++ ImageService/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 func main() {
-	
+}
+
+func SaveFile(b []byte, dName, fName string) (err error) {
+	fPath := filepath.Join(dName, fName)
+	fPath = filepath.Clean(fPath)
+	fs, er := os.Stat(dName)
+	if er != nil {
+		if os.IsNotExist(er) {
+			if err = os.MkdirAll(dName, 0755); err != nil {
+				return
+			}
+		} else {
+			return er
+		}
+	} else {
+		if !fs.IsDir() {
+			return fmt.Errorf("%s is not a directory", dName)
+		}
+	}
+	obj, err := os.Create(fPath)
+	if err != nil {
+		return
+	}
+	defer obj.Close()
+	if _, err = obj.Write(b); err != nil {
+		return
+	}
+	return
 }
